5621d7b9b010c174c591d125bb71bf7b0e8214ba
mmm <mmm>
2000-02-24 21:57:34 +0000

    *** empty log message ***

---
+++ cpn/cf.h
@@ -0,0 +1,11 @@
+// cf.h
+
+#ifndef _CF_H_
+#define _CF_H_
+
+#include "myvalarraydouble.h"
+
+extern void compareWithSoln( const double x, const valarray_double& y );
+extern void printVals( const double x, const valarray_double& y );
+
+#endif // _CF_H_
---
+++ cpn/derivs.h
@@ -0,0 +1,9 @@
+// derivs.h
+
+#ifndef _DERIVS_H_
+#define _DERIVS_H_
+
+#include "myvalarraydouble.h"
+extern valarray_double dydx( const double x, const valarray_double& y );
+
+#endif //_DERIVS_H_
