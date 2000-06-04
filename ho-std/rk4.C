3ea07be0bcb30bd4affff4cad9265cb2ce5b5102
mmm <mmm>
2000-06-04 23:18:37 +0000

    egcs-20000530 has apparently fixed the valarray problem.

--- ho-std/rk4.C
+++ ho-std/rk4.C
@@ -10,15 +10,9 @@ stepRk4( const double x, const valarray<double>& y, const double stepSize ) {
     
     valarray<double> k1 = stepSize * dydx( x, y );
 
-    //valarray<double> k2 = stepSize * dydx( x + stepSize/2.0, y + k1/2.0 );
-    valarray<double> tmp = k1/2.0;
-    tmp += y;
-    valarray<double> k2 = stepSize * dydx( x + stepSize/2.0, tmp );
+    valarray<double> k2 = stepSize * dydx( x + stepSize/2.0, y + k1/2.0 );
 
-    //valarray<double> k3 = stepSize * dydx( x + stepSize/2.0, y + k2/2.0 );
-    tmp = k2/2.0;     
-    tmp += y;
-    valarray<double> k3 = stepSize * dydx( x + stepSize/2.0, tmp );
+    valarray<double> k3 = stepSize * dydx( x + stepSize/2.0, y + k2/2.0 );
 
     valarray<double> k4 = stepSize * dydx( x + stepSize, y + k3 );
 
